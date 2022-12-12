8a2dca2d6e6783a09bb21d2fb55faa4eddecf52c
Denis Githuku <githukudenis@gmail.com>
2022-12-12 21:48:52 +0300

    Add todo viewmodel, save task test Add task screen implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,6 +81,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.5.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.0-alpha03")
 
     // coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
@@ -94,13 +95,22 @@ dependencies {
     implementation("androidx.hilt:hilt-work:1.0.0")
     testImplementation("junit:junit:4.13.2")
     kapt("androidx.hilt:hilt-compiler:1.0.0")
-    implementation("com.google.dagger:hilt-android:2.44")
+    implementation("com.google.dagger:hilt-android:2.44.2")
     kapt("com.google.dagger:hilt-android-compiler:2.44.2")
 
     // datastore prefs
     implementation("androidx.datastore:datastore-preferences-core:1.0.0")
 
     implementation("androidx.savedstate:savedstate-ktx:1.2.0")
+
+    // api sdk version less than 24
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.0")
+
+    // date time picker
+    implementation("io.github.vanpra.compose-material-dialogs:datetime:0.8.1-rc")
+
+    // accompanist
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.28.0")
 
     // room
     implementation("androidx.room:room-runtime:2.4.3")
@@ -111,9 +121,11 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
 
     implementation("androidx.activity:activity-compose:1.6.1")
-    implementation("androidx.compose.ui:ui:1.3.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.3.1")
-    implementation("androidx.compose.material3:material3:1.1.0-alpha02")
+    implementation("androidx.compose.ui:ui:1.3.2")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.3.2")
+    implementation("androidx.compose.animation:animation:1.3.2")
+    implementation("androidx.compose.ui:ui-tooling:1.3.2")
+    implementation("androidx.compose.material3:material3:1.1.0-alpha03")
 
     // Local Unit Tests
     implementation("androidx.test:core:1.5.0")
@@ -123,7 +135,7 @@ dependencies {
     testImplementation("org.robolectric:robolectric:4.9")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
     testImplementation("com.google.truth:truth:1.1.3")
-    testImplementation("org.mockito:mockito-core:2.28.2")
+    testImplementation("org.mockito:mockito-core:4.8.0")
     testImplementation("app.cash.turbine:turbine:0.12.1")
 
     // Instrumented Unit Tests
@@ -134,10 +146,10 @@ dependencies {
     androidTestImplementation("com.google.truth:truth:1.1.3")
     androidTestImplementation("androidx.test.ext:junit:1.1.4")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
-    androidTestImplementation("org.mockito:mockito-core:2.28.2")
+    androidTestImplementation("org.mockito:mockito-core:4.8.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.4")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.3.1")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.3.1")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.3.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.3.2")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.3.2")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.3.2")
 }
--- app/src/main/java/com/githukudenis/todoey/MainActivity.kt
+++ app/src/main/java/com/githukudenis/todoey/MainActivity.kt
@@ -1,42 +1,29 @@
 package com.githukudenis.todoey
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.*
+import androidx.navigation.compose.rememberNavController
 import com.githukudenis.todoey.ui.theme.TodoeyTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             TodoeyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
+                val navHostController = rememberNavController()
+
+                TodoeyNavigator(
+                    navController = navHostController
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TodoeyTheme {
-        Greeting("Android")
     }
 }
