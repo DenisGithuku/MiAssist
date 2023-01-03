909df626bbd3697152c425adf4c6888c2ae7875c
Denis Githuku <githukudenis@gmail.com>
2023-01-03 14:49:42 +0300

    move drawable resources to tasks

--- app/src/main/java/com/githukudenis/todoey/TodoeyNavigator.kt
+++ app/src/main/java/com/githukudenis/todoey/TodoeyNavigator.kt
@@ -5,9 +5,9 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.githukudenis.tasks.TodoeyScreen
-import com.githukudenis.tasks.add_task.AddTaskScreen
-import com.githukudenis.tasks.task_detail.TaskDetailScreen
-import com.githukudenis.tasks.task_list.TaskListScreen
+import com.githukudenis.tasks.ui.add_task.AddTaskScreen
+import com.githukudenis.tasks.ui.task_detail.TaskDetailScreen
+import com.githukudenis.tasks.ui.task_list.TaskListScreen
 
 @Composable
 fun TodoeyNavigator(
@@ -15,32 +15,32 @@ fun TodoeyNavigator(
 ) {
     NavHost(navController = navController, startDestination = com.githukudenis.tasks.TodoeyScreen.TaskList.routeId) {
         composable(route = com.githukudenis.tasks.TodoeyScreen.TaskList.routeId) {
-            com.githukudenis.tasks.task_list.TaskListScreen(
+            TaskListScreen(
                 onNewTask = {
-                    navController.navigate(route = com.githukudenis.tasks.TodoeyScreen.AddTask.routeId) {
+                    navController.navigate(route = TodoeyScreen.AddTask.routeId) {
                         launchSingleTop = true
-                        popUpTo(com.githukudenis.tasks.TodoeyScreen.AddTask.routeId) {
+                        popUpTo(TodoeyScreen.AddTask.routeId) {
                             inclusive = true
                         }
                     }
                 },
                 onOpenTodoDetails = { taskId ->
-                    navController.navigate(com.githukudenis.tasks.TodoeyScreen.TaskDetail.routeId + "/$taskId") {
+                    navController.navigate(TodoeyScreen.TaskDetail.routeId + "/$taskId") {
                         launchSingleTop = true
-                        popUpTo(com.githukudenis.tasks.TodoeyScreen.TaskDetail.routeId) {
+                        popUpTo(TodoeyScreen.TaskDetail.routeId) {
                             inclusive = true
                         }
                     }
                 }
             )
         }
-        composable(route = com.githukudenis.tasks.TodoeyScreen.TaskDetail.routeId + "/{taskId}") {
-            com.githukudenis.tasks.task_detail.TaskDetailScreen(onUpdateTask = {
+        composable(route = TodoeyScreen.TaskDetail.routeId + "/{taskId}") {
+            TaskDetailScreen(onUpdateTask = {
                 navController.popBackStack()
             }, onNavigateUp = { navController.navigateUp() })
         }
-        composable(route = com.githukudenis.tasks.TodoeyScreen.AddTask.routeId) {
-            com.githukudenis.tasks.add_task.AddTaskScreen(
+        composable(route = TodoeyScreen.AddTask.routeId) {
+            AddTaskScreen(
                 onSaveTask = {
                     navController.popBackStack()
                 },
